931c2ac941806248536ee8d82e1abca5880302be
Brian Li <brian.li96@yahoo.com>
2020-10-31 05:46:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,10 +1,11 @@
 def find_min_in_nested_arrays(src)
-  # puts src[0].sort[0]
-  # minArray = []
-  # i = 0
+  puts src[0].sort[0]
+  minArray = []
+  i = 0
   while i < src.length do
-    # minArray.push(src[i].sort[0])
+    minArray.push(src[i].sort[0])
+    i += 1
   end
-  # return minArray
+  return minArray
 end
 
